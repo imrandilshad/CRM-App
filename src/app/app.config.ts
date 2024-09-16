e0f77f503f80c2d445f827244cc536bca1be2a17
mohsin5450 <mian.mohsinali343@gmail.com>
2024-09-16 12:29:23 +0500

    remove the error of dupplicate import from app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
