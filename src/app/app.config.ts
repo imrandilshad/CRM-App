b86cbfc896d65a0ae2019d2d6918a7c6865a7557
mohsin5450 <mian.mohsinali343@gmail.com>
2024-09-17 23:47:08 +0500

    recover merge conflict code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
-<<<<<<<<< Temporary merge branch 1
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-=========
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { routes } from "./app.routes";
->>>>>>>>> Temporary merge branch 2
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+ import { ErrorInterceptor } from './shared/services/error-interceptor.service';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+    provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ]
 };
