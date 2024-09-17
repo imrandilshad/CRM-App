0b986379d95eec63830ecceffcf32fbdabe42880
imran-dilshad <imran.dilshad@ebitlogix.com>
2024-09-18 00:00:21 +0500

    improve the folder structure standars for guards and changed the absolute path

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,27 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { AuthGuard } from './../../../auth.guard';
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { Route, Router, RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   standalone: true,
-  imports: [RouterModule,CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  imports: [RouterModule, CommonModule],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.scss",
 })
 export class NavbarComponent {
-
-  constructor(private router: Router) {
-
-  }
-  islogin:boolean=localStorage.getItem('username')?true:false;
+  constructor(private router: Router) {}
+  islogin: boolean = localStorage.getItem("username") ? true : false;
   logOut() {
-    localStorage.removeItem('username')
-     window.location.reload()
-    this.router.navigate(["login"])
-    }
-
+    localStorage.removeItem("username");
+    window.location.reload();
+    this.router.navigate(["login"]);
+  }
 }
-
-
