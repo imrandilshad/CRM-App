6d08bc2886822b3dd793612825c0b4d003aba31f
mohsin5450 <mian.mohsinali343@gmail.com>
2024-09-23 12:15:58 +0500

    remove some unuse code

--- src/app/shared/models/registration.model.ts
+++ src/app/shared/models/registration.model.ts
@@ -1,8 +1,3 @@
-export interface Registration {
- username:string;
- email:string;
- password:string;
-}
 
 
 export interface User {
--- src/app/shared/services/registration.service.ts
+++ src/app/shared/services/registration.service.ts
@@ -1,54 +1,48 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Registration, User } from '../models/registration.model';
+import { User } from '../models/registration.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegistrationService {
-  // registrationData:Registration={
-  //   username: '',
-  //   email: '',
-  //   password: ''
-  // }
-    registrationData:User={
-      userId: 0,
-      firstName: "",
-      middleName: "D",
-      lastName: "xyz",
-      mobileNo: "1234567890",
-      emailId: "",
-      altMobileNo: "0987654321",
-      password: "",
-      userAddress: {
-        city: "",
-        state: "",
-        pincode: "000000",
-        addressLine: ""
-      },
-      userSocialDetails: {
-        facebookProfileUrl: "",
-        linkdinProfileUrl: "",
-        instagramHandle: "",
-        twitterHandle: ""
-      }
-  }
-
-
-  data = {
-    username: 'cbz123',
-    email: 'cbz@gmail.com',
-    password: '12345'
+  registrationData: User = {
+    userId: 0,
+    firstName: '',
+    middleName: 'D',
+    lastName: 'xyz',
+    mobileNo: '1234567890',
+    emailId: '',
+    altMobileNo: '0987654321',
+    password: '',
+    userAddress: {
+      city: '',
+      state: '',
+      pincode: '000000',
+      addressLine: '',
+    },
+    userSocialDetails: {
+      facebookProfileUrl: '',
+      linkdinProfileUrl: '',
+      instagramHandle: '',
+      twitterHandle: '',
+    },
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  // Register user method
-  registerUser(username:string,email:string,password:string ): Observable<any> {
-   this.registrationData.firstName=username;
-    this.registrationData.emailId=email;
-    this.registrationData.password=password;
-    return this.http.post('https://freeapi.miniprojectideas.com/api/JWT/CreateNewUser', this.registrationData);
+  registerUser(
+    username: string,
+    email: string,
+    password: string
+  ): Observable<any> {
+    this.registrationData.firstName = username;
+    this.registrationData.emailId = email;
+    this.registrationData.password = password;
+    return this.http.post(
+      'https://freeapi.miniprojectideas.com/api/JWT/CreateNewUser',
+      this.registrationData
+    );
   }
 }
